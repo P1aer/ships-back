fbaecb10c358072d6f68fa79fb1879b1b1d871f4
P1aer <arhoangel@bk.ru>
2022-10-03 00:00:21 +0300

    state event handler add, some code fixes

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ app.get("/rooms/:id",(req,res) => {
     const id = req.params.id
 
     if (rooms.has(id) && rooms.get(id)["users"].size <= 2) {
-        res.json(rooms.get(id))
+        res.json({
+            users: rooms.get(id).readyState
+        })
     }
     else res.json(null)
 })
@@ -61,13 +63,13 @@ app.post("/rooms", (req, res) => {
     if (!rooms.has(roomId)){
         rooms.set(
             roomId, {
-                'users':new Set([roomId]),
+                'users':new Set([]),
+                "readyState": {},
                 "public" : true,
                 "name": name
             }
         )
     }
-
     res.send();
 })
 
@@ -75,16 +77,49 @@ app.post("/rooms", (req, res) => {
 
 
 io.on("connection", (socket)=> {
-    socket.on('ROOM:JOIN',(roomId , cb ) => {
+    socket.on('ROOM:JOIN',(roomId) => {
+        const room =  rooms.get(roomId)
+        if (!room) {
+            return
+        }
+        if (room['users'].size >= 2 ) {
+            return;
+        }
         socket.join(roomId);
-        rooms.get(roomId)['users'].add(socket.id);
-        const users = [...rooms.get(roomId)["users"].values()];
-        cb()
-        socket.to(roomId).emit("ROOM:PREPARATION",users);
+        room['users'].add(socket.id);
+        room.readyState[socket.id] = false
+        const users = [...room["users"].values()];
+        socket.to(roomId).emit("ROOM:JOINED",room.readyState);
     })
 
-    socket.on("GAME:TURN",() => {
+    socket.on("GAME:TURN",(roomId) => {
 
+    })
+
+    socket.on('ROOM:LEAVE',(roomId, userId) => {
+        const room = rooms.get(roomId)
+        if (!room) {
+            return;
+        }
+        if (!room['users'].has(userId)) {
+            return;
+        }
+        room['users'].delete(userId)
+        if (room['users'].size === 0) {
+            rooms.delete(roomId)
+            return
+        }
+        delete room.readyState[socket.id]
+        const users = [...room["users"].values()];
+        socket.to(roomId).emit("ROOM:LEAVED",room.readyState);
+    })
+
+    socket.on('ROOM:STATE',(roomId, state) => {
+/*        console.log(roomId)
+        console.log(rooms)*/
+        const room = rooms.get(roomId)
+        room.readyState[socket.id] = state
+        socket.to(roomId).emit("ROOM:STATE",room.readyState);
     })
 
     socket.on("disconnect",() => {
@@ -96,8 +131,9 @@ io.on("connection", (socket)=> {
                 map.delete(roomId)
                 return;
             }
+            delete obj.readyState[socket.id]
             const users = [...obj["users"].values()];
-            socket.to(roomId).emit("ROOM:SET_USERS",users);
+            socket.to(roomId).emit("ROOM:LEAVED",obj.readyState);
         })
     })
 })
